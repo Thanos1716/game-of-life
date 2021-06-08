fbe6c962be27eff3412b93497fdde7f8e0846aa2
Thanos1716 <thanecdsmith@gmail.com>
2021-06-09 00:17:55 +0100

    Reorganised and added F to reset iter speed

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 
 int min(x, y) {
-    if (x > y) {
-        return y;
-    }
+    if (x > y) return y;
     return x;
 }
 
@@ -13,7 +11,7 @@ int main() {
     const int cellWidth           = 10;
     const int cellHeight          = cellWidth;
     const int cellCountVertical   = 70;
-    const int cellCountHorizontal = 110;
+    const int cellCountHorizontal = 100;
     const int cellPadding         = 1;
     const int screenWidth         = (cellWidth  + cellPadding) * cellCountHorizontal + cellPadding;
     const int screenHeight        = (cellHeight + cellPadding) * cellCountVertical   + cellPadding;
@@ -28,97 +26,40 @@ int main() {
         }
     }
 
-    bool running = true;
-    bool mode    = true;
-    bool next    = false;
-    bool text    = false;
-    float totalTimeOfFrame = 0.1f;
-    float timeUntilNextFrame = totalTimeOfFrame;
+    bool running   = true;
+    bool mode      = true;
+    bool next      = false;
+    bool text      = false;
+    bool hideZeros = true;
+    float frameTotal = 0.1f;
+    float frameRemaining = frameTotal;
 
     while (!WindowShouldClose()) {
 
-        if (IsKeyPressed(KEY_SPACE)) running = !running;
-
-        if (IsKeyDown(KEY_UP))   totalTimeOfFrame /= 1.02;
-        if (IsKeyDown(KEY_DOWN)) totalTimeOfFrame *= 1.02;
-
-        timeUntilNextFrame -= GetFrameTime();
-
-        for (int y = 0; y < cellCountVertical; ++y) {
-            for (int x = 0; x < cellCountHorizontal; ++x) {
-                if (board[y][x] == true) {
-                    DrawRectangle(x * (cellWidth + cellPadding) + cellPadding, y * (cellHeight + cellPadding) + cellPadding, cellWidth, cellHeight, WHITE);
-                } else {
-                    DrawRectangle(x * (cellWidth + cellPadding) + cellPadding, y * (cellHeight + cellPadding) + cellPadding, cellWidth, cellHeight, DARKGRAY);
-                }
-            }
-        }
-
-        if (IsKeyPressed(KEY_RIGHT)) next = true;
-
-        if (timeUntilNextFrame < 0 || next/*running || next*/) { /// HJHLJKHKJHLHJHLJLHLHKJHJLHJH***********
-            bool nextBoard[cellCountVertical][cellCountHorizontal];
-            for (int y = 0; y < cellCountVertical; ++y) {
-                for (int x = 0; x < cellCountHorizontal; ++x) {
-                    nextBoard[y][x] = board[y][x];
-                }
-            }
-
-            for (int y = 0; y < cellCountVertical; ++y) {
-                for (int x = 0; x < cellCountHorizontal; ++x) {
-                    int count = 0;
-                    for (int i = -1; i < 2; ++i) {
-                        for (int j = -1; j < 2; ++j) {
-                            if (board[y + j][x + i] && !(i == 0 && j == 0) && 0 <= x + i && x + i < cellCountHorizontal && 0 <= y + j && y + j < cellCountVertical) {
-                                count++;
-                            }
-                        }
-                    }
-
-                    if (board[y][x]) { // if cell is alive
-                        if ((count > 3 || count < 2) && (running || next)) {
-                            nextBoard[y][x] = false; // die
-                        }
-
-                    }
-                    else { // if cell is dead
-                        if (count == 3 && (running || next)) {
-                            nextBoard[y][x] = true; // be born
-                        }
-                    }
-
-
-                if (text) DrawText(TextFormat("%i", count), x * (cellWidth + cellPadding) + cellPadding + cellWidth/4, y * (cellHeight + cellPadding) + cellPadding, min(cellWidth, cellHeight), BLACK);
-                }
-            }
-
-            for (int y = 0; y < cellCountVertical; ++y) {
-                for (int x = 0; x < cellCountHorizontal; ++x) {
-                    board[y][x] = nextBoard[y][x];
-                }
-            }
-            timeUntilNextFrame = totalTimeOfFrame;
-        }
-        next = false;
-
-        BeginDrawing();
-        ClearBackground(BLACK);
-
+        // Get the coordinates of the cell the mouse is closest to
         int mouseX = (GetMouseX() - cellPadding / 2) / (cellWidth  + cellPadding);
         int mouseY = (GetMouseY() - cellPadding / 2) / (cellHeight + cellPadding);
 
+        // On left click set the mode based on the cell the mouse is over
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            if (board[mouseY][mouseX]) {
-                mode = false;
-            } else {
-                mode = true;
-            }
+            if (board[mouseY][mouseX]) mode = false;
+            else mode = true;
         }
 
-        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-            board[mouseY][mouseX] = mode;
-        }
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) board[mouseY][mouseX] = mode;
 
+        // Increase/decrease the frame speed
+        if (IsKeyDown(KEY_UP))   frameTotal /= 1.02f;
+        if (IsKeyDown(KEY_DOWN)) frameTotal *= 1.02f;
+
+        if (running) frameRemaining -= GetFrameTime();
+
+        // Key checks & updates
+        if (IsKeyPressed(KEY_SPACE)) running = !running;
+        if (IsKeyPressed(KEY_T)) text        = !text;
+        if (IsKeyPressed(KEY_Z)) hideZeros   = !hideZeros;
+        if (IsKeyPressed(KEY_RIGHT)) next    = true;
+        if (IsKeyPressed(KEY_F)) frameTotal  = 0.1f;
         if (IsKeyPressed(KEY_C)) {
             for (int y = 0; y < cellCountVertical; ++y) {
                 for (int x = 0; x < cellCountHorizontal; ++x) {
@@ -127,7 +68,58 @@ int main() {
             }
         }
 
-        if (IsKeyPressed(KEY_T)) text = !text;
+        BeginDrawing();
+
+        ClearBackground(DARKGRAY);
+
+        // Draw the cells
+        for (int y = 0; y < cellCountVertical; ++y) {
+            for (int x = 0; x < cellCountHorizontal; ++x) {
+                DrawRectangle(x * (cellWidth + cellPadding) + cellPadding, y * (cellHeight + cellPadding) + cellPadding, cellWidth, cellHeight, board[y][x] ? RED : BLACK);
+            }
+        }
+
+        bool nextBoard[cellCountVertical][cellCountHorizontal];
+        for (int y = 0; y < cellCountVertical; ++y) {
+            for (int x = 0; x < cellCountHorizontal; ++x) {
+
+                int count = 0;
+                nextBoard[y][x] = board[y][x];
+
+                // Count up surrounding live cells
+                for (int i = -1; i < 2; ++i) {
+                    for (int j = -1; j < 2; ++j) {
+                        if (board[y + j][x + i] && !(i == 0 && j == 0) && 0 <= x + i &&
+                            x + i < cellCountHorizontal && 0 <= y + j & y + j < cellCountVertical) count++;
+                    }
+                }
+
+                // Update next board to next iteration based on state of current board
+                if (frameRemaining <= 0 || next) {
+                    if (board[y][x]) {
+                        if (count > 3 || count < 2) nextBoard[y][x] = false;
+                    } else {
+                        if (count == 3) nextBoard[y][x] = true;
+                    }
+                }
+
+                // Draw number of adjacent live cells on cell
+                if (text && !(hideZeros && count == 0)) DrawText(TextFormat("%i",
+                    count), x * (cellWidth + cellPadding) + cellPadding + cellWidth / 4,
+                    y * (cellHeight + cellPadding) + cellPadding, min(cellWidth, cellHeight), LIGHTGRAY); // cellwidth / 4 is arbitary, TODO: find better value
+                }
+            }
+
+            for (int y = 0; y < cellCountVertical; ++y) {
+                for (int x = 0; x < cellCountHorizontal; ++x) {
+                    board[y][x] = nextBoard[y][x];
+                }
+            }
+
+
+        if (frameRemaining <= 0) frameRemaining = frameTotal;
+
+        next = false;
 
         EndDrawing();
     }
